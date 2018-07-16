10c98b6fb9bd2a89d603cba2be76b0267af468bb
Kristen Papke <klpapke215@gmail.com>
2018-07-16 03:27:20 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -122,6 +122,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 puts programmer_hash
 end
